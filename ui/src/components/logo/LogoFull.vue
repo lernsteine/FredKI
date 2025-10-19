cea73ffffd45030f3597b6fc64ee0eb00f962275
LERNSTEINE® <ralf.hagemeister@lernsteine.de>
2025-10-19 16:07:34 +0200

    Update logo

--- ui/src/components/logo/LogoFull.vue
+++ ui/src/components/logo/LogoFull.vue
@@ -55,7 +55,7 @@
         />
       </g>
     </svg>
-    <img v-else src="@/assets/logo/MaxKB-logo.svg" :height="height" />
+    <img v-else src="@/assets/logo/fredki_logo.png" :height="height" />
   </template>
 </template>
 <script setup lang="ts">
