5844350ae236b704132625a3aafdfdf5c5962ac4
dionea-dev <220576@gmx.de>
2025-11-07 11:48:11 +0100

    Change title from 'MaxKB' to 'FredKI'

--- ui/src/views/system-setting/theme/index.vue
+++ ui/src/views/system-setting/theme/index.vue
@@ -298,7 +298,7 @@ const themeForm = ref<any>({
   icon: '',
   loginLogo: '',
   loginImage: '',
-  title: 'MaxKB',
+  title: 'FredKI',
   slogan: t('theme.defaultSlogan'),
   ...defaultPlatformSetting,
 })
