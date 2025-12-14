3275557e8cec300852aaa594c362c060322470d2
dionea-dev <220576@gmx.de>
2025-12-14 20:39:42 +0100

    Remove unused import for applicationWorkflow

--- ui/src/locales/lang/de-DE/views/index.ts
+++ ui/src/locales/lang/de-DE/views/index.ts
@@ -10,13 +10,11 @@ import workspace from './workspace'
 import application from './application'
 import problem from './problem'
 import applicationOverview from './application-overview'
-import applicationWorkflow from './application-workflow'
 import paragraph from './paragraph'
 import chatLog from './chat-log'
 import chatUser from './chat-user'
 import operateLog from './operate-log'
 import shared from './shared'
-
 export default {
   login,
   model,
@@ -30,7 +28,6 @@ export default {
   application,
   problem,
   applicationOverview,
-  applicationWorkflow,
   paragraph,
   chatLog,
   chatUser,
