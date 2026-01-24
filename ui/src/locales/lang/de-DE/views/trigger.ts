5f07260cdc76e6d75ca47754dacb5163b2008d6d
dionea-dev <220576@gmx.de>
2026-01-24 06:42:27 +0100

    Add German translations for trigger view

--- ui/src/locales/lang/de-DE/views/trigger.ts
+++ ui/src/locales/lang/de-DE/views/trigger.ts
@@ -1,1 +1,19 @@
-
+export default {
+  title: 'Auslöser',
+  task: 'Aufgabe',
+  type: {
+    scheduled: 'Zeitgesteuerter Auslöser',
+    scheduledDesc: 'Aufgaben monatlich, wöchentlich, täglich oder in Intervallen ausführen',
+    event: 'Ereignis-Auslöser',
+    eventDesc: 'Aufgaben ausführen, wenn ein bestimmtes Ereignis eintritt',
+  },
+  createTrigger: 'Auslöser erstellen',
+  editTrigger: 'Auslöser bearbeiten',
+  from: {
+    triggerName: {
+      label: 'Name des Auslösers',
+      placeholder: 'Bitte geben Sie den Namen des Auslösers ein',
+      requiredMessage: 'Bitte geben Sie den Namen des Auslösers ein',
+    },
+  },
+}
