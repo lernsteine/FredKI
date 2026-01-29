b0e3337060c4eb18bd57729b641cee77421f9de2
dionea-dev <220576@gmx.de>
2026-01-29 08:25:33 +0100

    Update German translations in workspace.ts

--- ui/src/locales/lang/de-DE/views/workspace.ts
+++ ui/src/locales/lang/de-DE/views/workspace.ts
@@ -1,18 +1,18 @@
 export default {
   title: 'Arbeitsbereich',
   list: 'Arbeitsbereichsliste',
-  name: 'Arbeitsbereichsname',
-  toWorkspace: 'Zum Arbeitsbereich zurückkehren',
+  name: 'Name des Arbeitsbereichs',
+  toWorkspace: 'Zum Arbeitsbereich',
   delete: {
     confirmTitle: 'Löschung des Arbeitsbereichs bestätigen:',
     confirmContent:
-      'Nach dem Löschen werden alle Mitglieder in diesem Bereich entfernt. Bitte vorsichtig vorgehen.',
+      'Nach der Löschung werden alle Mitglieder dieses Arbeitsbereichs entfernt. Bitte vorsichtig vorgehen.',
     confirmContentNotDelete:
-      'Dieser Arbeitsbereich enthält Wissensbasis-Ressourcen und Agent-Ressourcen und kann nicht gelöscht werden.',
+      'Dieser Arbeitsbereich enthält Wissensbasis-Ressourcen und Agenten-Ressourcen und kann nicht gelöscht werden.',
   },
   member: {
     delete: {
-      confirmTitle: 'Entfernung des Mitglieds bestätigen:',
+      confirmTitle: 'Entfernen des Mitglieds bestätigen:',
     },
   },
 }
