713b612b30f4db43de0c35944ed6ac699ce81eaf
dionea-dev <220576@gmx.de>
2026-05-29 04:47:59 +0200

    Rename 'chat' import to 'aiChat' in German locale

--- ui/src/locales/lang/de-DE/index.ts
+++ ui/src/locales/lang/de-DE/index.ts
@@ -5,7 +5,7 @@ import views from './views'
 import theme from './theme'
 import common from './common'
 import dynamicsForm from './dynamics-form'
-import chat from './ai-chat'
+import aiChat from './ai-chat'
 import workflow from './workflow'
 export default {
   lang: 'Deutsch',
@@ -16,6 +16,6 @@ export default {
   de,
   common,
   dynamicsForm,
-  chat,
+  aiChat,
   workflow
 }
